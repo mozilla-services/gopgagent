da4f24003cbec8ef26fdd0761e40eab273a02832
Julien Vehent [:ulfr] <jvehent@users.noreply.github.com>
2016-08-18 19:02:28 -0400

    Update gpgagent_test.go

--- gpgagent_test.go
+++ gpgagent_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package gpgagent /* import "mig.ninja/mig/pgp/gpgagent" */
+package gopgagent /* import "go.mozilla.org/gopgagent" */
 
 import (
 	"fmt"
